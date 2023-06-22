00d3087788aa590fd038755d2ab2224877ccc53e
Vassilij <v.misenko@web.de>
2023-06-22 15:58:50 +0200

    css active und js erstellt

---
+++ scripts/slideshow.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', (event) {
+
+    let buttons = document.querySelectorAll('[data-button-js]')
+
+    buttons.addEventListener('click', (event) => {
+        //TODO: Logik von Copilot -> es fehlt die Logik in HTML/ CSS für active
+       
+    })
+
+})
